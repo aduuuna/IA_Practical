27528bf539773b029bb81bc62a4d191a8ddb4908
Owusu Ansah Joy <owusujoyansah@gmail.com>
2023-12-18 09:24:19 +0000

    connection failed

--- modules/productModules.js
+++ modules/productModules.js
@@ -1,38 +1,36 @@
 const mongoose = require("mongoose");
 
-const productSchema = mongoose.Schema({
-    {
-        PatientID:{
-            type: String,
-            required: true
-        },
-        PatientName:{
-            type: String,
-            required: true
-        },
-        PatientAge:{
-            type: String,
-            required: true
-        },
-        PatientGender:{
-            type: String,
-            required: true
-        },
-        PatientAddress:{
-            type: String,
-            required: true
-        },
-        PatientPhoneNumber:{
-            type: String,
-            required: true
-        },
-
-        
+const productSchema = mongoose.Schema(
+  {
+    PatientID: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true
-    }
+    PatientName: {
+      type: String,
+      required: true,
+    },
+    PatientAge: {
+      type: String,
+      required: true,
+    },
+    PatientGender: {
+      type: String,
+      required: true,
+    },
+    PatientAddress: {
+      type: String,
+      required: true,
+    },
+    PatientPhoneNumber: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const Product = mongoose.model("Product", productSchema);  
-mongoose.model.exports = Product; 
+const Product = mongoose.model("Product", productSchema);
+mongoose.model.exports = Product;
--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const Product = require("./modules/productModules");
 
 app.get("/", (req, res) => {
   res.send({ message: "Welcome to the health care application." });
 });
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000.");
 });
 
+app.post("/product", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json({ product });
+  } catch {
+    console.log("Error");
+    res.status(500).json({ message: "error message" });
+  }
+});
 mongoose
   .connect(
     "mongodb+srv://owusujoyansah:123456admin@cluster0.5dv0gxc.mongodb.net/"
